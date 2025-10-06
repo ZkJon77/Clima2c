e1e3284d9d060d2bf3a0201d8f54dfe92bc25189
jonatan <extremeshow046@gmail.com>
2025-10-06 09:20:51 -0300

    Add files via upload

---
+++ Clima2c/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './App.css'
+import { CloudSun} from 'lucide-react';
+
+function App() {
+  const [cidade, setCidade] = useState('');
+  const [clima, setClima] = useState(null);
+  const [carregando, setCarregando] = useState(false);
+  const [erro, setErro] = useState('');
+
+  //função para buscar dados do clima
+  const buscarClima = async () => {
+    //validação do campo vazio da cidade
+    if (!cidade.trim()){
+      setErro('Por favor, digite uma cidade');
+      return;
+    }
+
+    setCarregando(true);
+    setErro('');
+
+    try{
+
+    }catch (error){
+
+    }finally{
+      
+    }
+  }
+  return (
+    <>
+     <div className="container">
+      <div className="content">
+        <header>
+          <h1>
+          <CloudSun color="white" size={48} />
+            Consulta de clima</h1>
+          <p>Exemplo de consumo de API com React</p>
+        </header>
+        <div className="busca-box">
+          <div className="busca-container">
+            <input type="text" />
+            <button></button>
+            </div>
+        </div>
+      </div>
+     </div> 
+    </>
+  )
+}
+
+export default App
