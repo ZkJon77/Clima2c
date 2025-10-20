9346b38b9a5a38fe7b4ac129ecf546276bfce2d6
jonatan <extremeshow046@gmail.com>
2025-10-20 09:44:02 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,23 @@ function App() {
     setErro('');
 
     try{
+      const API_KEY = "";
+      const url ="";
+      const resposta = await fetch(url);
+
+      if (!resposta.ok){
+        throw new Error('Cidade nao encontrada');
+        setClima(dados);
+      }
+    }
+    {catch(error){
+      setErro(error.message);
+      setClima(null);
+    }finally{
+      setCarregando(false);
+    }
+
+    }
 
     }catch (error){
 
