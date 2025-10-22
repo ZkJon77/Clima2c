475fe4e94a3c48310dee5cd90313d00cd6a0673e
jonatan <extremeshow046@gmail.com>
2025-10-22 11:45:24 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { CloudSun, MapPinned } from 'lucide-react';
+import { CloudSun, MapPinned, Thermometer, Wind,Bubbles } from 'lucide-react';
 
 function App() {
   const [cidade, setCidade] = useState('');
@@ -19,72 +19,119 @@ function App() {
     setCarregando(true);
     setErro('');
 
+// Try Executa os comandos
     try{
-      const API_KEY = "";
-      const url ="";
-      const resposta = await fetch(url);
+const API_KEY = "";
+const url = "";
+const resposta = await frtch(url)
 
-      if (!resposta.ok){
-        throw new Error('Cidade nao encontrada');
-        setClima(dados);
-      }
-    }
-    {catch(error){
-      setErro(error.message);
-      setClima(null);
-    }finally{
-      setCarregando(false);
-    }
+if(!resposta.ok){
+  throw new Error ("Cidade não encontrada");
+}
 
-    }
+const dados = await resposta.json();
+setClima(dados);
+
 
     }catch (error){
-
+      setErro(erro.mensage);
+      setClima(null);
     }finally{
-      
+      setCarregando(false)
     }
   }
   return (
     <>
-     <div className="container">
-      <div className="content">
-        <header>
-          <h1>
-          <CloudSun color="white" size={48} />
-            Consulta de clima</h1>
-          <p>Exemplo de consumo de API com React</p>
-        </header>
-        {/* caixa de busca */}
-        <div className="busca-box">
-          <div className="busca-container">
-            <input type="text"
-            placeholder='Digite o nome da cidade..'
-            />
-            <button>Buscar</button>
+      <div className="container">
+        <div className="content">
+          <header>
+            <h1>
+              <CloudSun color="blue" size={48} />
+              Consulta de Clima
+            </h1>
+            <p>Exemplo de consumo de API com react </p>
+          </header>
+
+          {/* caixa de busca */}
+          <div className="busca-box">
+            <div className="busca-container">
+              <input
+               type="text"
+               placeholder="Digite o nome da cidade.."
+               />
+               <button>Buscar</button>
             </div>
             </div>
-            {/* resultado do clima */}
+
+            {/* resultado do Clima */}
             <div id="card-resultado">
               <div id="cidade-info">
                 <div id="cidade-nome">
-                <MapPinned color="blue" size={48} />
-                  Campinas,BR
+                <MapPinned style={{color: '#000000ff'}} size={48}/>
+                Campinas, BR
                 </div>
                 <p id="cidade-desc">
-                  nublado
+                  Nublado
                 </p>
-              </div>
+              </div> {/* fecha #cidade-desc */}
+
+              {/* temperatura parcial */}
               <div id="temperatura-box">
                 <div id="temp-valor">21°C</div>
                 <div id="temp-sens">
-                  Sensação térmica: 21°C
+                  Sensação Térmica: 21°C
                 </div>
               </div>
-            </div>
 
-        
+              <div id="detales-box">
+                {/* inicio Temperatura */}
+                <div className="detal-item">
+                   <div className="detal-item">
+                   <Thermometer />
+                </div>
+                <p className="detal-texto">
+                  Min/Max
+                </p>
+                <p className="detal-valor">
+                  23°C/27°C
+                </p>
+                </div>
+              </div>{/* fim Temperatura */}
+              <div id="detales-box">
+                {/*inicio umidade */}
+                <div className="detal-item">
+                   <div className="detal-item">
+                   <Bubbles />
+                </div>
+                <p className="detal-texto">
+                  Min/Max
+                </p>
+                <p className="detal-valor">
+                 12%
+                </p>
+                </div>
+              </div>{/* fim umidade */}
+
+              <div id="detales-box">
+               {/* inicio vento */}
+                <div className="detal-item">
+                   <div className="detal-item">
+                     <Wind/>
+                </div>
+                <p className="detal-texto">
+                  Min/Max
+                </p>
+                <p className="detal-valor">
+                 12km/h
+                </p>
+                </div>
+              </div>  {/* fim vento */}
+
+            </div>{/* fecha #card-resultado */}
+
+          
+        </div>
       </div>
-     </div> 
     </>
   )
 }
