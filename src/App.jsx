165bc26f051140421bf24bef5f42971271860c9d
jonatan <extremeshow046@gmail.com>
2025-10-30 08:56:04 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { CloudSun, MapPinned, Thermometer, Wind,Bubbles } from 'lucide-react';
+import { CloudSun, MapPinned, Thermometer, Droplet, Wind } from 'lucide-react';
 
 function App() {
   const [cidade, setCidade] = useState('');
@@ -19,117 +19,127 @@ function App() {
     setCarregando(true);
     setErro('');
 
-// Try Executa os comandos
+    // Try Executa os comandos
     try{
-const API_KEY = "";
-const url = "";
-const resposta = await frtch(url)
+      const API_KEY = "50878f4678cd0841144b44b2fca0ccc0";
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API_KEY}&units=metric&lang=pt_br`;
+      const resposta = await fetch(url);
 
-if(!resposta.ok){
-  throw new Error ("Cidade não encontrada");
-}
+      if(!resposta.ok){
+        throw new Error('Cidade não encontrada');
+      }
 
-const dados = await resposta.json();
-setClima(dados);
-
+      const dados = await resposta.json();
+      setClima(dados);
 
     }catch (error){
-      setErro(erro.mensage);
+      setErro(error.message);
       setClima(null);
     }finally{
-      setCarregando(false)
+      setCarregando(false);
     }
-  }
+  };
+const handleykepress = (e) =>{
+  if (e.key === "enter")
+    buscarClima();
+}
+
+
   return (
     <>
       <div className="container">
         <div className="content">
           <header>
             <h1>
-              <CloudSun color="blue" size={48} />
+              <CloudSun color="white" size={48} />
               Consulta de Clima
             </h1>
-            <p>Exemplo de consumo de API com react </p>
+            <p>Exemplo de consumo de API com React</p>
           </header>
-
-          {/* caixa de busca */}
+          
+          {/* Caixa de Busca */}
           <div className="busca-box">
             <div className="busca-container">
-              <input
-               type="text"
-               placeholder="Digite o nome da cidade.."
-               />
-               <button>Buscar</button>
+              <input 
+                type="text"
+                placeholder="Digite o nome da cidade.."
+                value={cidade}
+                onChange={(e) => setCidade(e.target.value)}
+                onKeyPress={(handleykepress)}
+              />
+              <button
+              onClick={buscarClima}
+              disabled={carregando}
+              
+              >
+              {carregando ? "Buscando..." :"Buscar"}        
+                </button>
             </div>
-            </div>
+          </div>
 
-            {/* resultado do Clima */}
-            <div id="card-resultado">
-              <div id="cidade-info">
-                <div id="cidade-nome">
-                <MapPinned style={{color: '#000000ff'}} size={48}/>
+          {/* Resultado do Clima */}
+          <div id="card-resultado">
+            <div id="cidade-info">
+              <div id="cidade-nome">
+                <MapPinned style={{color: '#550808ff'}} size={48} />
                 Campinas, BR
-                </div>
-                <p id="cidade-desc">
-                  Nublado
-                </p>
-              </div> {/* fecha #cidade-desc */}
-
-              {/* temperatura parcial */}
-              <div id="temperatura-box">
-                <div id="temp-valor">21°C</div>
-                <div id="temp-sens">
-                  Sensação Térmica: 21°C
-                </div>
               </div>
+              <p id="cidade-desc">
+                Nublado
+              </p>
+            </div> {/* Fecha #cidade-desc*/}
 
-              <div id="detales-box">
-                {/* inicio Temperatura */}
-                <div className="detal-item">
-                   <div className="detal-item">
-                   <Thermometer />
+            {/* Temperatura principal */}
+            <div id="temperatura-box">
+              <div id="temp-valor">21°C</div>
+              <div id="temp-sens">
+                Sensação Térmica: 21°C
+              </div>
+            </div>
+
+            <div id="detalhes-box">
+              
+              <div className="detal-item">{/* Inicio Temperatura */}
+                <div className="detal-icone">
+                  <Thermometer />
                 </div>
                 <p className="detal-texto">
                   Min/Max
                 </p>
                 <p className="detal-valor">
-                  23°C/27°C
+                  23ºC/27ºC
                 </p>
-                </div>
-              </div>{/* fim Temperatura */}
-              <div id="detales-box">
-                {/*inicio umidade */}
-                <div className="detal-item">
-                   <div className="detal-item">
-                   <Bubbles />
+              </div>{/* Fim Temperatura */}
+
+              <div className="detal-item">{/* Inicio Umidade */}
+                <div className="detal-icone">
+                  <Droplet />
                 </div>
                 <p className="detal-texto">
-                  Min/Max
+                  Umidade
                 </p>
                 <p className="detal-valor">
-                 12%
+                  12%
                 </p>
-                </div>
-              </div>{/* fim umidade */}
+              </div>{/* Fim Umidade */}
 
-              <div id="detales-box">
-               {/* inicio vento */}
-                <div className="detal-item">
-                   <div className="detal-item">
-                     <Wind/>
+              <div className="detal-item">{/* Inicio Vento */}
+                <div className="detal-icone">
+                  <Wind />
                 </div>
                 <p className="detal-texto">
-                  Min/Max
+                  Vento
                 </p>
                 <p className="detal-valor">
-                 12km/h
+                  12 km/h
                 </p>
-                </div>
-              </div>  {/* fim vento */}
+              </div>{/* Fim Vento */}
 
-            </div>{/* fecha #card-resultado */}
+            </div>
 
-          
+
+          </div> {/* Fecha #card-resultado */}
+
         </div>
       </div>
     </>
